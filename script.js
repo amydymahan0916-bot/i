3d05c5e6db5bc0887014e0dbcd2a9d150a44f518
ماهان <amydymahan0916@gmail.com>
2026-07-03 11:19:34 +0330

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log('MAHAN');
+// دکمه کپی اینستاگرام
+function copyID() {
+    const input = document.getElementById("insta");
+
+    navigator.clipboard.writeText(input.value).then(() => {
+
+        const btn = document.querySelector(".copy-box button");
+
+        btn.innerText = "کپی شد ✓";
+
+        setTimeout(() => {
+            btn.innerText = "کپی";
+        }, 2000);
+
+    }).catch(() => {
+        alert("کپی انجام نشد.");
+    });
+}
+
+
+// سوالات متداول
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+
+    question.addEventListener("click", () => {
+
+        const item = question.parentElement;
+
+        item.classList.toggle("active");
+
+    });
+
+});
